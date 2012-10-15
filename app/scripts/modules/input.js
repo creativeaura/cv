9f787caeee6dc14c1caa7fc22a2cf59f609cd86f
Gaurav Jassal <gauravjassal@gmail.com>
2012-10-15 20:11:26 +0100

    History added to input commands

--- app/scripts/modules/input.js
+++ app/scripts/modules/input.js
@@ -1,37 +1,52 @@
 /*global $, jQuery, define, alert, require, window, Backbone */
 /*jslint browser:true, devel:true, unused:false */
 
-define([
-	'backbone',
-  'app'
-	],
-function(Backbone, App) {
-	'use strict';
+define(['backbone', 'app'], function(Backbone, App) {
+  'use strict';
 
   var Input = {};
 
-	Input.View = Backbone.View.extend({
-		manage: true,
-		template: 'input',
+  Input.View = Backbone.View.extend({
+    manage: true,
+    template: 'input',
     events: {
-      'keydown #inputfield' : 'action',
-      'blur #inputfield'    : 'setFocus'
+      'keydown #inputfield': 'action',
+      'blur #inputfield': 'setFocus'
     },
 
     initialize: function() {
       this.prompt = '[gaurav$bash]';
+      this.commandHistory = [];
+      this.commandPointer = 0;
     },
 
     serialize: function() {
-      return {prompt: this.prompt};
+      return {
+        prompt: this.prompt
+      };
     },
 
     action: function(event) {
       var command;
-      if (event.which === 13) {
+      if(event.which === 13) {
         command = this.$input.val();
         this.$input.val('');
-        App.trigger('command', {p: this.prompt, 'c': command});
+        App.trigger('command', {
+          p: this.prompt,
+          'c': command
+        });
+        this.commandHistory.push(command);
+        this.commandPointer = this.commandHistory.length;
+      } else if(event.which === 38) {
+        if(this.commandPointer > 0) {
+          this.$input.val(this.commandHistory[this.commandPointer - 1]);
+          this.commandPointer--;
+        }
+      } else if(event.which === 40) {
+        if(this.commandPointer < this.commandHistory.length) {
+          this.$input.val(this.commandHistory[this.commandPointer - 1]);
+          this.commandPointer++;
+        }
       }
     },
     afterRender: function() {
@@ -41,8 +56,7 @@ function(Backbone, App) {
     setFocus: function() {
       this.$input.focus();
     }
+  });
 
-	});
-
-	return Input;
+  return Input;
 });
