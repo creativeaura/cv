4dfa3e5631d21cf204c05a27c24d0e4d4552c130
Gaurav Jassal <gauravjassal@gmail.com>
2013-05-23 16:48:01 +0200

    adding build script
    The script runs requirejs build script and generates a compiled javscript with all dependencies, copies the app folder to deploy and update the source javascript file from the compiled javascript.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+echo -e "\033[36m Building the deployment package. Please wait ...\033[0m"
+
+START=$(date +%s)
+
+r.js -o app.build.js optimize=none
+rm -rf deploy
+mkdir deploy
+cp -R app/* deploy/
+rm deploy/styles/*.scss
+rm -rf deploy/scripts/plugins
+rm -rf deploy/scripts/modules
+rm deploy/scripts/app.js
+rm deploy/scripts/config.js
+rm deploy/scripts/main.js
+rm deploy/scripts/router.js
+rm deploy/scripts/social.js
+rm deploy/scripts/libs/almond.js
+rm deploy/scripts/libs/backbone.js
+rm deploy/scripts/libs/handlebars.js
+rm deploy/scripts/libs/jquery.js
+rm deploy/scripts/libs/lodash.js
+rm deploy/config.rb
+
+sed '/<!-- build:js scripts\/amd-app.js -->/,/<!-- endbuild -->/ { s/config.js/main-built.js/ } ' < deploy/index.html > deploy/index_new.html
+
+rm deploy/index.html
+mv deploy/index_new.html deploy/index.html
+
+END=$(date +%s)
+
+DIFF=$(( ($END - $START)))
+
+echo -e "\033[33m It took ${DIFF} seconds to finish the build process.\033[0m"
